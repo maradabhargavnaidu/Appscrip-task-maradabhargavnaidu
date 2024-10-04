09eb57604a7c01b12740050a13a295d94119d200
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2024-10-05 00:42:49 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,20 @@ import LogoIcon from "@/app/icons/LogoIcons";
 import ProfileIcon from "@/app/icons/ProfileIcons";
 import SearchIcon from "@/app/icons/SearchIcon";
 import ShoppingBagIcon from "@/app/icons/ShoppingBagIcon";
+import Hamburger from "@/app/icons/Hamburger";
 const Navbar = () => {
   return (
     <nav className="main-nav container">
       <LogoIcon />
       <h2>LOGO</h2>
-      <div className="nav-right-items">
+      <div className="nav-right-items-desk">
         <SearchIcon />
         <HeartIcon />
         <ShoppingBagIcon />
         <ProfileIcon />
+      </div>
+      <div className="nav-right-items-mob">
+        <Hamburger />
       </div>
     </nav>
   );
